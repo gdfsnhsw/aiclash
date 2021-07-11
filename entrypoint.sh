1ead1fe6668e24fbd90a606a851416808c63d1cb
Zack <81725003+gdfsnhsw@users.noreply.github.com>
2021-07-11 19:43:22 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,16 +11,24 @@ fi
 
 # 开启转发，需要 privileged
 # Deprecated! 容器默认已开启
-# echo "1" > /proc/sys/net/ipv4/ip_forward
+echo "1" > /proc/sys/net/ipv4/ip_forward
+
+if [ ! -e '/clash_config/dashboard/index.html' ] || [ "$UPDATE" = "true" ] ; then
+    mkdir -p /root/.config/clash/dashboard
+    unzip -d /root/.config/clash/ /root/.config/clash/gh-pages.zip
+    mv -f /root/.config/clash/clash-dashboard-gh-pages/* /root/.config/clash/dashboard
+    cp -r /root/.config/clash/dashboard /clash_config/dashboard
+    echo -e "\033[32m=======更新dashboard成功=======\033[0m"
+fi
 
 if [ ! -e '/clash_config/config.yaml' ]; then
-    echo "init /clash_config/config.yaml"
     cp  /root/.config/clash/config.yaml /clash_config/config.yaml
+    echo -e "\033[32m=======更新config.yaml成功=======\033[0m"
 fi
 
 if [ ! -e '/clash_config/Country.mmdb' ]; then
-    echo "init /clash_config/Country.mmdb"
     cp  /root/.config/clash/Country.mmdb /clash_config/Country.mmdb
+    echo -e "\033[32m=======更新Country.mmdb成功=======\033[0m"   
 fi
 
 exec "$@"
