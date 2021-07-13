efb6e0e6a15f1bdb6c7c282b98dba4317a4fe9ae
Zack <81725003+gdfsnhsw@users.noreply.github.com>
2021-07-13 17:10:53 +0800

    Update setup-redir-tun.sh

--- scripts/setup-redir-tun.sh
+++ scripts/setup-redir-tun.sh
@@ -56,3 +56,31 @@ set_ipt() {
 
     fireqos start
 }
+
+
+set_nft() {
+    source /usr/lib/clash/common.sh
+
+    while true; do
+        ip link show $PROXY_TUN_DEVICE_NAME
+        [ $? -eq 0 ] && break
+        sleep 1
+    done
+
+    ip tuntap add utun mode tun user root
+    ip link set utun up
+    ip addr add "198.18.0.1/16" dev utun
+
+    ip route replace default dev utun table 114
+
+    ip rule del fwmark 114514 lookup 114 > /dev/null 2> /dev/null
+    ip rule add fwmark 114514 lookup 114
+    
+    nft -f /usr/lib/clash/nft_redir-tun.conf
+    
+    #sysctl -w net/ipv4/ip_forward=1
+
+    ip addr
+
+    fireqos start
+}
