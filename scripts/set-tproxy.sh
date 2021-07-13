f980de705d9a66d96f9292e1c07d2ec5d9316cdc
Zack <81725003+gdfsnhsw@users.noreply.github.com>
2021-07-13 21:29:17 +0800

    Update set-tproxy.sh

--- scripts/set-tproxy.sh
+++ scripts/set-tproxy.sh
@@ -1,1 +1,11 @@
 
+    . /usr/lib/clash/common.sh
+
+    ip route replace local default dev lo table "$IPROUTE2_TABLE_ID"
+
+    ip rule del fwmark "$NETFILTER_MARK" lookup "$IPROUTE2_TABLE_ID" > /dev/null 2> /dev/null
+    ip rule add fwmark "$NETFILTER_MARK" lookup "$IPROUTE2_TABLE_ID"
+
+    nft -f /usr/lib/clash/tproxy.conf
+    
+   # sysctl -w net/ipv4/ip_forward=1
