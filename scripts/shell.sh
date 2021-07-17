17a4078526d2673f525336794233c08778464db1
Zack <81725003+gdfsnhsw@users.noreply.github.com>
2021-07-18 03:58:54 +0800

    Update shell.sh

--- scripts/shell.sh
+++ scripts/shell.sh
@@ -18,9 +18,9 @@ while true; do
     sleep 3
 done
 
-while true; do
-    ip link show utun
-    [ $? -eq 0 ] && break
-    echo -e "\033[32m正在启动clash，请等待...\033[0m"
-    sleep 3
-done
+#while true; do
+#    ip link show utun
+#    [ $? -eq 0 ] && break
+#    echo -e "\033[32m正在启动clash，请等待...\033[0m"
+#    sleep 3
+#done
