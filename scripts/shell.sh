54342128276dfbd912fa136750111e9e44908dd5
Zack <81725003+gdfsnhsw@users.noreply.github.com>
2021-07-18 01:27:30 +0800

    Update shell.sh

--- scripts/shell.sh
+++ scripts/shell.sh
@@ -4,3 +4,5 @@ source /usr/lib/clash/common.sh
 
 sed -i '/url=*/'d $formyairportPath
 setconfig url $URL
+
+wget "http://127.0.0.1:25500/getprofile?name=profiles/formyairport.ini&token=password" -O clash_config.yaml
