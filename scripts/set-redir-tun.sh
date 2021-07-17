f744377767d0fa230bf114231b14bfeb11a59655
Zack <81725003+gdfsnhsw@users.noreply.github.com>
2021-07-17 20:32:31 +0800

    Update set-redir-tun.sh

--- scripts/set-redir-tun.sh
+++ scripts/set-redir-tun.sh
@@ -1,33 +1,50 @@
 #!/bin/bash
 
-source /usr/lib/clash/common.sh
-
-while true; do
-    ip link show utun
-    [ $? -eq 0 ] && break
-    echo -e "\033[32m正在启动clash，请等待...\033[0m"
-    sleep 3
-done
-
-ip tuntap add utun mode tun user nobody
-ip link set utun up
-ip addr add "198.18.0.1/16" dev utun
-
-ip route replace default dev utun table 114
-
-ip rule del fwmark 114514 lookup 114 > /dev/null 2> /dev/null
-ip rule add fwmark 114514 lookup 114
-
-
 _setup(){
 
-nft -f /usr/lib/clash/nft_redir-tun.conf
+    while true; do
+        ip link show utun
+        [ $? -eq 0 ] && break
+        echo -e "\033[32m正在启动clash，请等待...\033[0m"
+        sleep 3
+    done
+
+    ip tuntap add utun mode tun user nobody
+    ip link set utun up
+    ip addr add "198.18.0.1/16" dev utun
+
+    ip route replace default dev utun table 114
+
+    ip rule del fwmark 114514 lookup 114 > /dev/null 2> /dev/null
+    ip rule add fwmark 114514 lookup 114
+
+    nft -f /usr/lib/clash/nft_redir-tun.conf
+
+    ip addr
+    
+    #sysctl -w net/ipv4/ip_forward=1
 
     exit 0
 
 }
 
 _setup2(){
+
+    while true; do
+        ip link show utun
+        [ $? -eq 0 ] && break
+        echo -e "\033[32m正在启动clash，请等待...\033[0m"
+        sleep 3
+    done
+
+    ip tuntap add utun mode tun user nobody
+    ip link set utun up
+    ip addr add "198.18.0.1/16" dev utun
+
+    ip route replace default dev utun table 114
+
+    ip rule del fwmark 114514 lookup 114 > /dev/null 2> /dev/null
+    ip rule add fwmark 114514 lookup 114
 
     nft -f - << EOF
     define LOCAL_SUBNET = { 10.0.0.0/8, 127.0.0.0/8, 169.254.0.0/16, 172.16.0.0/12, 192.168.0.0/16, 224.0.0.0/4, 240.0.0.0/4 }
@@ -53,14 +70,15 @@ _setup2(){
     }
 EOF
 
+    ip addr
+    
     #sysctl -w net/ipv4/ip_forward=1
 
     exit 0
 
 }
 
-
-
-#sysctl -w net/ipv4/ip_forward=1
-
-ip addr
+case "$1" in
+"setup") _setup;;
+"setup2") _setup2;;
+esac
