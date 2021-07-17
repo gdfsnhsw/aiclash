bdc33f615fbc35f40b73c43f099d434f18aeef5e
Zack <81725003+gdfsnhsw@users.noreply.github.com>
2021-07-18 03:05:30 +0800

    Update shell.sh

--- scripts/shell.sh
+++ scripts/shell.sh
@@ -13,3 +13,10 @@ while true; do
     echo -e "\033[32m正在启动subconverter，请等待...\033[0m"
     sleep 3
 done
+
+while true; do
+    ip link show utun
+    [ $? -eq 0 ] && break
+    echo -e "\033[32m正在启动clash，请等待...\033[0m"
+    sleep 3
+done
