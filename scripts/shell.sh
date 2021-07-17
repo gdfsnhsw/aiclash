bc3b8abbcb4239470c19955591843e02a4a81103
Zack <81725003+gdfsnhsw@users.noreply.github.com>
2021-07-18 03:45:48 +0800

    Update shell.sh

--- scripts/shell.sh
+++ scripts/shell.sh
@@ -11,9 +11,10 @@ setconfig udp $UDP
 while true; do
     supervisorctl status subconverter
     [ $? -eq 0 ] && \
+    [ ! -e '/clash_config/config.yaml' ] && \
     wget "http://127.0.0.1:25500/getprofile?name=profiles/formyairport.ini&token=password" -O config.yaml && \
     break
-    echo -e "\033[32m正在启动subconverter，请等待...\033[0m"
+    echo -e "\033[32m正在启动subconverter并生成clash配置文件，请等待...\033[0m"
     sleep 3
 done
 
