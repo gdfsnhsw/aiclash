3d8b94a04569052fa0b3b072eb4fb102dbf373ab
Zack <81725003+gdfsnhsw@users.noreply.github.com>
2021-07-18 02:48:53 +0800

    Update shell.sh

--- scripts/shell.sh
+++ scripts/shell.sh
@@ -5,4 +5,10 @@ source /usr/lib/clash/common.sh
 sed -i '/url=*/'d $formyairportPath
 setconfig url $URL
 
-wget "http://127.0.0.1:25500/getprofile?name=profiles/formyairport.ini&token=password" -O clash_config.yaml
+
+while true; do
+    supervisorctl status subconverter
+    [ $? -eq 0 ] && wget "http://127.0.0.1:25500/getprofile?name=profiles/formyairport.ini&token=password" -O config.yaml
+    echo -e "\033[32m正在启动subconverter，请等待...\033[0m"
+    sleep 3
+done
