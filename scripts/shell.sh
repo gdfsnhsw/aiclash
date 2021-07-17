07c6df22b362dbd42213e8044a6aecfe8acabacf
Zack <81725003+gdfsnhsw@users.noreply.github.com>
2021-07-18 03:00:12 +0800

    Update shell.sh

--- scripts/shell.sh
+++ scripts/shell.sh
@@ -7,7 +7,9 @@ setconfig url $URL
 
 while true; do
     supervisorctl status subconverter
-    [ $? -eq 0 ] && wget "http://127.0.0.1:25500/getprofile?name=profiles/formyairport.ini&token=password" -O config.yaml && break
+    [ $? -eq 0 ] && \
+    wget "http://127.0.0.1:25500/getprofile?name=profiles/formyairport.ini&token=password" -O config.yaml && \
+    break
     echo -e "\033[32m正在启动subconverter，请等待...\033[0m"
     sleep 3
 done
