e9c3390f41b892db1bb9341125c23b1ccd2141be
Zack <81725003+gdfsnhsw@users.noreply.github.com>
2021-07-18 01:25:38 +0800

    Update common.sh

--- scripts/common.sh
+++ scripts/common.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+formyairportPath=/etc/subconverter/profiles/formyairport.ini
+
+setconfig(){
+	#参数1代表变量名，参数2代表变量值,参数3即文件路径
+	[ -z "$3" ] && configpath=$formyairportPath || configpath=$3
+	[ -n "$(grep ${1} $configpath)" ] && sed -i "s#${1}=.*#${1}=${2}#g" $configpath || echo "${1}=${2}" >> $configpath
+}
+
 log() {
   echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $*"
 }
