1810f56c066738865eb65fffe8deee7bbd917536
Zack <81725003+gdfsnhsw@users.noreply.github.com>
2021-07-18 16:45:43 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,22 +9,7 @@ echo "1" > /proc/sys/net/ipv4/ip_forward
 echo 'nameserver 223.5.5.5'>>/etc/resolv.conf
 apk add supervisor
 
-echo -e "\033[32m======================== 2. 自定义路由表 ============================\033[0m"
-if [ "$ROUTE_MODE" = "redir-tun" ]; then
-    echo -e "混合模式"
-    /usr/lib/clash/set-redir-tun.sh set1 &
-elif [ "$ROUTE_MODE" = "redir-tun2" ]; then
-    echo -e "混合模式"
-    /usr/lib/clash/set-redir-tun.sh set2 &
-elif [ "$ROUTE_MODE" = "tun" ]; then
-    echo -e "tun模式"
-    /usr/lib/clash/set-tun.sh set1 &
-elif [ "$ROUTE_MODE" = "tproxy" ]; then
-    echo -e "tproxy模式"
-    /usr/lib/clash/set-tproxy.sh set1 &
-fi
-
-echo -e "\033[32m======================== 3. 载入所需文件 ============================\033[0m"
+echo -e "\033[32m======================== 2. 载入所需文件 ============================\033[0m"
 if [ ! -e '/etc/subconverter/subconverter' ] ; then
     tar -zxvf /root/.config/subconverter/subconverter.tar.gz -C /etc/
     cp  /root/.config/subconverter/profiles/* /etc/subconverter/profiles
@@ -69,6 +54,21 @@ fi
 if [ ! -e '/etc/mosdns/geosite.dat' ]; then
     cp  /root/.config/mosdns/geosite.dat /etc/mosdns/geosite.dat
     echo -e "载入geosite.dat"   
+fi
+
+echo -e "\033[32m======================== 3. 自定义路由表 ============================\033[0m"
+if [ "$ROUTE_MODE" = "redir-tun" ]; then
+    echo -e "混合模式"
+    /usr/lib/clash/set-redir-tun.sh set1 &
+elif [ "$ROUTE_MODE" = "redir-tun2" ]; then
+    echo -e "混合模式"
+    /usr/lib/clash/set-redir-tun.sh set2 &
+elif [ "$ROUTE_MODE" = "tun" ]; then
+    echo -e "tun模式"
+    /usr/lib/clash/set-tun.sh set1 &
+elif [ "$ROUTE_MODE" = "tproxy" ]; then
+    echo -e "tproxy模式"
+    /usr/lib/clash/set-tproxy.sh set1 &
 fi
 
 echo -e "\033[32m======================== 4. 启动程序 ===============================\033[0m"
