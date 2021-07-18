3fdc05bb697fcda98cbd99981155cfc52294293d
Zack <81725003+gdfsnhsw@users.noreply.github.com>
2021-07-19 01:19:12 +0800

    Update getconfig.sh

--- scripts/getconfig.sh
+++ scripts/getconfig.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-source /usr/lib/clash/common.sh
+formyairportPath=/etc/subconverter/profiles/formyairport.ini
+
+setconfig(){
+	#参数1代表变量名，参数2代表变量值,参数3即文件路径
+	[ -z "$3" ] && configpath=$formyairportPath || configpath=$3
+	[ -n "$(grep ${1} $configpath)" ] && sed -i "s#${1}=.*#${1}=${2}#g" $configpath || echo "${1}=${2}" >> $configpath
+}
 
 sed -i '/url=*/'d $formyairportPath
 setconfig url $URL
