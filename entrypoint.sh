c2de0546fb78346b11d4a8443002efba7c6de022
Zack <81725003+gdfsnhsw@users.noreply.github.com>
2021-07-20 20:51:06 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,9 +11,9 @@ apk add supervisor
 
 echo -e "\033[32m======================== 2. 载入所需文件 ============================\033[0m"
 if [ ! -e '/etc/subconverter/subconverter' ] ; then
-    tar -zxvf /root/.config/subconverter/subconverter.tar.gz -C /aiclash/
-    cp  /root/.config/subconverter/profiles/* /aiclash/subconverter/profiles
-    mv -f /aiclash/subconverter/profiles/all_base.tpl /aiclash/subconverter/base/all_base.tpl
+    tar -zxvf /root/.config/subconverter/subconverter.tar.gz -C /etc/
+    cp  /root/.config/subconverter/profiles/* /etc/subconverter/profiles
+    mv -f /etc/subconverter/profiles/all_base.tpl /etc/subconverter/base/all_base.tpl
     echo -e "载入subconverter"
 fi
 
@@ -21,38 +21,38 @@ if [ ! -e '/clash_config/dashboard/index.html' ] || [ "$UPDATE" = "true" ] ; the
     mkdir -p /root/.config/clash/dashboard
     unzip -d /root/.config/clash/ /root/.config/clash/gh-pages.zip
     mv -f /root/.config/clash/clash-dashboard-gh-pages/* /root/.config/clash/dashboard
-    cp -r /root/.config/clash/dashboard /aiclash/clash/dashboard
+    cp -r /root/.config/clash/dashboard /clash_config/dashboard
     echo -e "载入dashboard"
     
 fi
 
 #if [ ! -e '/clash_config/config.yaml' ]; then
-#    cp  /root/.config/clash/config.yaml /aiclash/clash/config.yaml
+#    cp  /root/.config/clash/config.yaml /clash_config/config.yaml
 #    echo -e "载入clash_config.yaml成功"
 #fi
 
 #if [ ! -e '/clash_config/Country.mmdb' ]; then
-#    cp  /root/.config/clash/Country.mmdb /aiclash/clash/Country.mmdb
+#    cp  /root/.config/clash/Country.mmdb /clash_config/Country.mmdb
 #    echo -e "载入Country.mmdb"   
 #fi
 
 if [ ! -e '/clash_config/getconfig.sh' ]; then
-    cp  /usr/lib/clash/getconfig.sh /aiclash/getconfig.sh
+    cp  /usr/lib/clash/getconfig.sh /clash_config/getconfig.sh
     echo -e "载入shell.sh"   
 fi
 
 if [ ! -e '/etc/mosdns/config.yaml' ]; then
-    cp  /root/.config/mosdns/config.yaml /aiclash/mosdns/config.yaml
+    cp  /root/.config/mosdns/config.yaml /etc/mosdns/config.yaml
     echo -e "载入mosdns_config.yaml"   
 fi
 
 if [ ! -e '/etc/mosdns/geoip.dat' ]; then
-    cp  /root/.config/mosdns/geoip.dat /aiclash/mosdns/geoip.dat
+    cp  /root/.config/mosdns/geoip.dat /etc/mosdns/geoip.dat
     echo -e "载入geoip.dat"   
 fi
 
 if [ ! -e '/etc/mosdns/geosite.dat' ]; then
-    cp  /root/.config/mosdns/geosite.dat /aiclash/mosdns/geosite.dat
+    cp  /root/.config/mosdns/geosite.dat /etc/mosdns/geosite.dat
     echo -e "载入geosite.dat"   
 fi
 
@@ -83,7 +83,7 @@ echo -e "supervisord启动成功..."
 
 echo -e "\033[32m======================== 5. 是否自动生成config ======================\033[0m"
 if [ $GET_CONFIG = true ]; then
-    bash /aiclash/getconfig.sh
+    bash /clash_config/getconfig.sh
     echo -e "\033[32m===成功生成clash配置文件===\033[0m"
 elif [ $GET_CONFIG = false ]; then
     echo -e "\033[32m===未设置生成config===\033[0m"
