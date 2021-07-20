25e95a08928022f192d039059eea92c16adf0e7a
Zack <81725003+gdfsnhsw@users.noreply.github.com>
2021-07-20 23:21:34 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,6 @@ set -e
 echo -e "\033[32m======================== 1. 安装修改系统文件 =========================\033[0m"
 mkdir -p /aiclash/clash
 mkdir -p /aiclash/mosdns
-mkdir -p /aiclash/subconverter
 # 开启转发，需要 privileged
 # Deprecated! 容器默认已开启
 echo "1" > /proc/sys/net/ipv4/ip_forward
