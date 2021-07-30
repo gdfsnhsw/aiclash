b44ce4afa66d1e4004c777c19306e5e2e602b6bc
Zack <81725003+gdfsnhsw@users.noreply.github.com>
2021-07-30 23:36:15 +0800

    Update set-tproxy.sh

--- scripts/set-tproxy.sh
+++ scripts/set-tproxy.sh
@@ -50,7 +50,6 @@ setup(){
     ip rule add fwmark 114514 lookup 114
 
     nft -f - << EOF
-    flush ruleset
     define LOCAL_SUBNET = { 10.0.0.0/8, 127.0.0.0/8, 169.254.0.0/16, 172.16.0.0/12, 192.168.0.0/16, 224.0.0.0/4, 240.0.0.0/4 }
     table clash
     flush table clash
